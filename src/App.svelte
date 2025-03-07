c3ff21828ad012cd050a10fc5a3b6ddc00b2a67a
Lucas <169063936+mustbeperfect@users.noreply.github.com>
2025-03-07 14:45:32 -0800

    Added duplicate checks for equipment and skills

--- src/App.svelte
+++ src/App.svelte
@@ -1,138 +1,240 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import ollama from 'ollama'
-  import { onMount } from 'svelte';
+    import ollama from 'ollama'
+    import {onMount} from 'svelte';
 
-  let npc = {
-      name: "Name",
-      race: "Race",
-      sex: "Sex",
-      description: "Description",
+    //AI system
+    let currentModel = 'No Model Selected';
+    let modelNames: string[] = [];
+    let showModelDropdown = false;
+    let noModel = false;
+    let isGenerating = false;
 
-      strength: 50,
-      stamina: 50,
-      dexterity: 50,
-      response: 50,
-      intuition: 50,
-      logic: 50,
-      personality: 50,
-      leadership: 50,
+    async function ollamaModels() {
+        const ollamaModel = await ollama.list();
+        modelNames = ollamaModel.models.map((model: any) => model.name);
+    }
 
-      height: 5,
-      weight: 5,
-      hitPoints: 5,
-      age: 5,
-      walk: 5,
-      run: 5,
-      credits: 5,
+    function selectModel(button: any) {
+        console.log(button)
+        currentModel = button;
+        showModelDropdown = false;
+    }
 
-      skills: [] as string[],
-      inventory: [] as string[],
-      weapons: [] as string[],
-      skillLevel: [] as string[],
-  };
+    function modelDropdown() {
+        showModelDropdown = !showModelDropdown;
+    }
 
-  let difficulty = 4;
-  let activeDifficultyButton = null;
-  let numInventory = 3;
-  let numSkills = 3;
-  let numWeapons = 3;
-  let customDescriptionActive = false;
-  let customDescription = "";
-  let descriptionLength = 100;
+    function ejectModel() {
+        currentModel = 'No Model Selected';
+    }
 
-  const raceTypes = ["Human", "Dralisite", "Vrusk", "Yazarian"];
-  const sexTypes = ["Male", "Female"];
+    //NPC Stats
 
-  function statGenerator() {
-      let randomFactor = Math.pow(Math.random(), 1 / (difficulty / 10));
-      let stat: number;
-      stat = Math.floor(Math.random() * 100) + 1;
-      return stat;
-  }
+    let npc = {
+        name: "Name",
+        race: "Race",
+        sex: "Sex",
+        description: "Description",
 
-  function setDifficulty(num: number) {
-      difficulty = num;
-  }
+        strength: 50,
+        stamina: 50,
+        dexterity: 50,
+        response: 50,
+        intuition: 50,
+        logic: 50,
+        personality: 50,
+        leadership: 50,
 
-  async function generate() {
+        height: 5,
+        weight: 5,
+        hitPoints: 5,
+        age: 5,
+        walk: 5,
+        run: 5,
+        credits: 5,
 
-      npc.inventory = [];
-      npc.skills = [];
-      npc.weapons = [];
-      npc.description = "";
-      npc.name = "";
+        skills: [] as string[],
+        skillLevel: [] as number[],
+        inventory: [] as string[],
+        weapons: [] as string[],
+    };
 
-      const race = Math.floor(Math.random() * (raceTypes.length-1)) + 1;
-      npc.race = raceTypes[race];
+    //World stats
+    const raceTypes = ["Human", "Dralisite", "Vrusk", "Yazarian"];
+    const sexTypes = ["Male", "Female"];
 
-      const sex = Math.floor(Math.random()) + 1;
-      npc.sex = sexTypes[sex];
+    //Generation parameters
+    let difficulty = 10;
+    let numInventory = 3;
+    let numSkills = 3;
+    let numWeapons = 3;
+    let descriptionLength = 100;
+    let customDescription = "";
+    let descriptionPrompt = "";
+    let usedSkills = "";
+    let usedInventory = "";
+    let usedWeapons = "";
+    let randomRace = true;
+    let customRace = "";
 
-      npc.strength = statGenerator();
-      npc.stamina = statGenerator();
-      npc.dexterity = statGenerator();
-      npc.response = statGenerator();
-      npc.intuition = statGenerator();
-      npc.logic = statGenerator();
-      npc.personality = statGenerator();
-      npc.leadership = statGenerator();
+    //Numerical generators
+    function randomNum(min: number, max: number) {
+        if (min > max) {
+            [min, max] = [max, min];
+        }
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-      npc.height = Math.floor(Math.random() * 3) + 1;
-      npc.weight = Math.floor(Math.random() * (60 + 1)) + 40;
-      npc.hitPoints = Math.floor(Math.random() * (60 + 1)) + 40;
-      npc.age = Math.floor(Math.random() * (180 + 1)) + 20;
-      npc.walk = Math.floor(Math.random() * (70 + 1)) + 10;
-      npc.run = Math.floor(Math.random() * (70 + 1)) + 10;
+    function randomStat() {
+        let stat: number;
+        stat = Math.floor(Math.random() * 100) + 1;
+        return stat;
+    }
 
-      npc.credits = Math.floor(Math.random() * (100000 + 1)) + 100;
+    function weightedStat(difficulty: number) {
+        const exponent = 1 + (difficulty - 1) * 0.5;
+        const randomValue = Math.pow(Math.random(), exponent);
+        return Math.floor(randomValue * 100) + 1;
+    }
 
-      const namePrompt = "Generate a sci-fi first and last name for a " + npc.sex + "individual. Include nothing more. DO NOT SAY YOU ARE GENERATING A NAME. THE ONLY TEXT SHOULD BE A NAME.";
-      const lootPrompt = "Act as a scifi equipment generator. Generate the name of a piece equipment that could be in someone's inventory such as a welder. Include nothing more. KEEP YOUR RESPONSE UNDER FOUR WORDS.";
-      const skillPrompt = "Act as a skill generator for an NPC. Generate a skill and include nothing more. KEEP YOUR RESPONSE UNDER TWO WORDS.";
-      const weaponPrompt = "Act as a scifi weapon generator. Generate the name of a weapon that could be in someone's inventory such as a Laser Rifle. Include nothing more. KEEP YOUR RESPONSE UNDER FOUR WORDS.";
+    //Setters
+    function setDifficulty(num: number) {
+        difficulty = num;
+    }
 
-      try {
+    function setRace(race: string) {
+        customRace = race;
+        randomRace = false;
+    }
+
+    function setRaceRandom() {
+        randomRace = true;
+    }
+
+    function resetParameters() {
+
+    }
+
+    function randomParameters() {
+
+    }
+
+    //Generator
+    async function generate() {
+
+        npc.inventory = [];
+        npc.skills = [];
+        npc.weapons = [];
+        npc.description = "";
+        npc.name = "";
+        npc.skillLevel = [];
+
+        if (customRace == "Human") {
+            npc.race = "Human";
+        }
+        else if (customRace == "Dralisite") {
+            npc.race = "Dralisite";
+        }
+        else if (customRace == "Vrusk") {
+            npc.race = "Vrusk";
+        }
+        else if (customRace == "Yazarian") {
+            npc.race = "Yazarian";
+        }
+        else {
+            randomRace = true;
+        }
+
+        if (randomRace == true) {
+            npc.race = raceTypes[randomNum(0,raceTypes.length-1)];
+        }
+
+        npc.sex = sexTypes[Math.floor(Math.random()) + 1];
+
+        if (difficulty == 10)
+        {
+            npc.strength = randomStat();
+            npc.stamina = randomStat();
+            npc.dexterity = randomStat();
+            npc.response = randomStat();
+            npc.intuition = randomStat();
+            npc.logic = randomStat();
+            npc.personality = randomStat();
+            npc.leadership = randomStat();
+
+            npc.height = randomNum(1,3);
+            npc.weight = randomNum(20,100);
+            npc.hitPoints = randomNum(20,100);
+            npc.age = randomNum(15,200);
+            npc.walk = randomNum(10,70);
+            npc.run = randomNum(10,70);
+        }
+        else
+        {
+            npc.strength = weightedStat(difficulty);
+            npc.stamina = weightedStat(difficulty);
+            npc.dexterity = weightedStat(difficulty);
+            npc.response = weightedStat(difficulty);
+            npc.intuition = weightedStat(difficulty);
+            npc.logic = weightedStat(difficulty);
+            npc.personality = weightedStat(difficulty);
+            npc.leadership = weightedStat(difficulty);
+
+            npc.height = randomNum(1,3);
+            npc.weight = randomNum(20,100);
+            npc.hitPoints = randomNum(20,100);
+            npc.age = randomNum(15,200);
+            npc.walk = randomNum(10,70);
+            npc.run = randomNum(10,70);
+        }
+
+        npc.credits =randomNum(100,100000);
+
+        let namePrompt = "Generate a sci-fi first and last name for a " + npc.sex + "individual. Include nothing more. DO NOT SAY YOU ARE GENERATING A NAME. THE ONLY TEXT SHOULD BE A NAME.";
+        let lootPrompt = "Act as a scifi equipment generator. Generate the name of a piece equipment that could be in someone's inventory such as a welder. Include nothing more. KEEP YOUR RESPONSE UNDER FOUR WORDS.";
+        let skillPrompt = "Act as a skill generator for an NPC. Generate a skill and include nothing more. KEEP YOUR RESPONSE UNDER TWO WORDS.";
+        let weaponPrompt = "Act as a scifi weapon generator. Generate the name of a weapon that could be in someone's inventory such as a Laser Rifle. Include nothing more. KEEP YOUR RESPONSE UNDER FOUR WORDS.";
+
+        //Ollama
+        try {
           const name = await ollama.chat({
-              model: 'llama3.2:1b',
+              model: currentModel,
               messages: [{ role: 'user', content: namePrompt }],
               stream: true
           });
           for await (const part of name) {
               npc.name += part.message.content;
           }
-      } catch (error) {
+        } catch (error) {
           console.error("Error fetching response:", error);
-      }
+        }
 
-      let descriptionPrompt = "";
-
-      if (customDescription != "")
-      {
+        if (customDescription != "")
+        {
           descriptionPrompt = "Generate a background for a character within the Star Frontiers sci-fi RPG called " + npc.name + " who is a " + npc.race + ". Here are your specific instructions: " + customDescription + ". Generate and include nothing more than description and keep it under" + descriptionLength + "words. DO NOT SAY YOU ARE MAKING A DESCRIPTION. Only generate a background. Do not include stats.";
-      }
-      else {
+        }
+        else {
           descriptionPrompt = "Generate a background for a character within the Star Frontiers sci-fi RPG called " + npc.name + " who is a " + npc.race + ". Generate and include nothing more than description and keep it under" + descriptionLength + "words. DO NOT SAY YOU ARE MAKING A DESCRIPTION. Only generate a background. Do not include stats.";
-      }
+        }
 
-      try {
+        try {
           const description = await ollama.chat({
-              model: 'llama3.2:1b',
+              model: currentModel,
               messages: [{ role: 'user', content: descriptionPrompt }],
               stream: true
           });
           for await (const part of description) {
               npc.description += part.message.content;
           }
-      } catch (error) {
+        } catch (error) {
           console.error("Error fetching response:", error);
-      }
+        }
 
-      for (let i = 0; i < numInventory; i++) {
+        for (let i = 0; i < numInventory; i++) {
+            let usedInventory = "";
           try {
               const inventory = await ollama.chat({
-                  model: 'llama3.2:1b',
+                  model: currentModel,
                   messages: [{ role: 'user', content: lootPrompt }],
               });
 
@@ -140,12 +242,20 @@
           } catch (error) {
               console.error("Error fetching response:", error);
           }
-      }
+            usedInventory += npc.inventory[i] + "";
+            lootPrompt = "Act as a scifi equipment generator. Generate the name of a piece equipment that could be in someone's inventory such as a welder. Include nothing more. You cannot use the following equipment: " + usedInventory + ". KEEP YOUR RESPONSE UNDER FOUR WORDS.";
+            /*
+              for (let j = 0; j < npc.inventory.length; j++) {
+                  usedInventory += npc.inventory[j];
+              }
 
-      for (let i = 0; i < numSkills; i++) {
+             */
+        }
+
+        for (let i = 0; i < numSkills; i++) {
           try {
               const skills = await ollama.chat({
-                  model: 'llama3.2:1b',
+                  model: currentModel,
                   messages: [{ role: 'user', content: skillPrompt }],
               });
 
@@ -153,50 +263,30 @@
           } catch (error) {
               console.error("Error fetching response:", error);
           }
-      }
+            usedSkills += npc.skills[i] + "";
+            npc.skillLevel = [...npc.skillLevel,randomNum(1,6)];
+            skillPrompt = "Act as a skill generator for an NPC. Generate a skill and include nothing more. You cannot use the following skills: " + usedSkills + ". KEEP YOUR RESPONSE UNDER TWO WORDS.";
+        }
 
-      for (let i = 0; i < numWeapons; i++) {
-          try {
+        for (let i = 0; i < numWeapons; i++) {
+            try {
               const weapons = await ollama.chat({
-                  model: 'llama3.2:1b',
+                  model: currentModel,
                   messages: [{ role: 'user', content: weaponPrompt }],
               });
 
               npc.weapons = [...npc.weapons, weapons.message.content];
-          } catch (error) {
+            } catch (error) {
               console.error("Error fetching response:", error);
-          }
-      }
-  }
+            }
+            usedWeapons += npc.weapons[i] + "";
+            weaponPrompt = "Act as a scifi weapon generator. Generate the name of a weapon that could be in someone's inventory such as a Laser Rifle. Include nothing more. You cannot use the following weapons: " + usedWeapons + ". KEEP YOUR RESPONSE UNDER FOUR WORDS.";
+        }
+    }
 
-
-  //Experimental code
-
-  let currentModel = 'No Model Selected';
-  let showModelDropdown = false;
-  let modelWarn = false;
-  let isGenerating = false;
-  let modelNames: string[] = [];
-
-  //handle ollama models
-  async function ollamaModels() {
-      const ollamaModel = await ollama.list();
-      modelNames = ollamaModel.models.map((model: any) => model.name);
-  }
-
-  function selectModel(button: any) {
-      console.log(button)
-      currentModel = button
-      showModelDropdown = false;
-  }
-
-  function modelDropdown() {
-      showModelDropdown = !showModelDropdown;
-  }
-
-  onMount(() => {
+    onMount(() => {
       ollamaModels();
-  })
+    })
 
 </script>
 
@@ -319,10 +409,10 @@
                     </div>
                     <div class="h-px bg-gray-800"></div>
                     <div class="overflow-y-auto flex-y gap-2 h-full">
-                        {#each npc.skills as item}
+                        {#each npc.skills as skill, i}
                             <div class="w-full flex-x justify-between">
-                                <div>{item}</div>
-                                <div>2</div>
+                                <div>{skill}</div>
+                                <div>{npc.skillLevel[i]}</div>
                             </div>
                         {/each}
                     </div>
@@ -376,11 +466,11 @@
             <div class="h-px w-full bg-gray-800"></div>
             <div class="flex-y gap-4 m-4">
                 <div class="flex gap-4">
-                    <button on:click={modelDropdown()}  class="button-hybrid w-full rounded-none text-center font-semibold font-mono flex justify-center">{currentModel}</button>
+                    <button on:click={modelDropdown}  class="button-hybrid w-full rounded-none text-center font-semibold font-mono flex justify-center">{currentModel}</button>
 
-                    <a class="icon-hybrid rounded-none" href="" aria-label="Youtube">
+                    <button on:click={ejectModel} class="icon-hybrid rounded-none" aria-label="Youtube">
                         <i class="bi bi-eject"></i>
-                    </a>
+                    </button>
                 </div>
 
                 {#if showModelDropdown}
@@ -395,19 +485,38 @@
             <div class="h-px w-full bg-gray-800"></div>
             <div class="h-full flex flex-y m-4 gap-5 overflow-y-auto">
                 <div class="flex gap-3 flex-wrap">
-                    <button class="hybrid tag">Easy</button>
-                    <button class="hybrid tag">Medium</button>
-                    <button class="hybrid tag">Hard</button>
-                    <button class="hybrid tag">Nightmare</button>
-                    <button class="hybrid tag">God</button>
-                    <button class="hybrid tag">Random</button>
+                    <button class={difficulty === 1 ? 'tag-toggled' : 'tag'}
+                            on:click={() => setDifficulty(1)}>Easy
+                    </button>
+                    <button class={difficulty === 2 ? 'tag-toggled' : 'tag'}
+                            on:click={() => setDifficulty(2)}>Medium
+                    </button>
+                    <button class={difficulty === 3 ? 'tag-toggled' : 'tag'}
+                            on:click={() => setDifficulty(3)}>Hard
+                    </button>
+                    <button class={difficulty === 4 ? 'tag-toggled' : 'tag'}
+                            on:click={() => setDifficulty(4)}>Nightmare
+                    </button>
+                    <button class={difficulty === 5 ? 'tag-toggled' : 'tag'}
+                            on:click={() => setDifficulty(5)}>God
+                    </button>
+                    <button class={difficulty === 10 ? 'tag-toggled' : 'tag'}
+                            on:click={() => setDifficulty(10)}>Random
+                    </button>
                 </div>
                 <div class="flex gap-3 flex-wrap">
-                    <button class="hybrid tag">Human</button>
-                    <button class="hybrid tag">Yazarian</button>
-                    <button class="hybrid tag">Vrusk</button>
-                    <button class="hybrid tag">Dralasite</button>
-                    <button class="hybrid tag">Random</button>
+                    {#each raceTypes as race}
+                        <button
+                                class={customRace === race ? 'tag-toggled' : 'tag'}
+                                on:click={() => setRace(race)}>
+                            {race}
+                        </button>
+                    {/each}
+                    <button
+                            class={randomRace === true ? 'tag-toggled' : 'tag'}
+                            on:click={setRaceRandom}>
+                        Random
+                    </button>
                 </div>
                 <input type="text" id="first_name" class="label font-mono placeholder-gray-500 focus:outline-none" placeholder="Custom Description Prompt" bind:value={customDescription}>
 
