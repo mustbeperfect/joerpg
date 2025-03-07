8fc78f6193adbdf9cc727ad7ac1b58ca7e6d6aea
Lucas <169063936+mustbeperfect@users.noreply.github.com>
2025-03-06 16:51:10 -0800

    Added loot generation and mode selector

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import ollama from 'ollama'
+  import { onMount } from 'svelte';
 
   let npc = {
       name: "Name",
@@ -26,19 +27,28 @@
       run: 5,
       credits: 5,
 
-      skills: [],
-      inventory: []
+      skills: [] as string[],
+      inventory: [] as string[],
+      weapons: [] as string[],
+      skillLevel: [] as string[],
   };
 
   let difficulty = 4;
   let activeDifficultyButton = null;
+  let numInventory = 3;
+  let numSkills = 3;
+  let numWeapons = 3;
+  let customDescriptionActive = false;
+  let customDescription = "";
+  let descriptionLength = 100;
+
   const raceTypes = ["Human", "Dralisite", "Vrusk", "Yazarian"];
   const sexTypes = ["Male", "Female"];
 
   function statGenerator() {
       let randomFactor = Math.pow(Math.random(), 1 / (difficulty / 10));
-      const stat = Math.floor(randomFactor * 100) + 1;
-      //const stat = Math.floor(Math.random() * 100) + 1;
+      let stat: number;
+      stat = Math.floor(Math.random() * 100) + 1;
       return stat;
   }
 
@@ -47,6 +57,12 @@
   }
 
   async function generate() {
+
+      npc.inventory = [];
+      npc.skills = [];
+      npc.weapons = [];
+      npc.description = "";
+      npc.name = "";
 
       const race = Math.floor(Math.random() * (raceTypes.length-1)) + 1;
       npc.race = raceTypes[race];
@@ -73,31 +89,114 @@
       npc.credits = Math.floor(Math.random() * (100000 + 1)) + 100;
 
       const namePrompt = "Generate a sci-fi first and last name for a " + npc.sex + "individual. Include nothing more. DO NOT SAY YOU ARE GENERATING A NAME. THE ONLY TEXT SHOULD BE A NAME.";
+      const lootPrompt = "Act as a scifi equipment generator. Generate the name of a piece equipment that could be in someone's inventory such as a welder. Include nothing more. KEEP YOUR RESPONSE UNDER FOUR WORDS.";
+      const skillPrompt = "Act as a skill generator for an NPC. Generate a skill and include nothing more. KEEP YOUR RESPONSE UNDER TWO WORDS.";
+      const weaponPrompt = "Act as a scifi weapon generator. Generate the name of a weapon that could be in someone's inventory such as a Laser Rifle. Include nothing more. KEEP YOUR RESPONSE UNDER FOUR WORDS.";
 
       try {
           const name = await ollama.chat({
               model: 'llama3.2:1b',
               messages: [{ role: 'user', content: namePrompt }],
+              stream: true
           });
-
-          npc.name = name.message.content;
+          for await (const part of name) {
+              npc.name += part.message.content;
+          }
       } catch (error) {
           console.error("Error fetching response:", error);
       }
 
-      const descriptionPrompt = "Generate a very short background for a character within the Star Frontiers sci-fi RPG called " + npc.name + " who is a " + npc.race + ". Generate and include nothing more than description and keep it under 100 words. DO NOT SAY YOU ARE MAKING A DESCRIPTION. Only generate a background. Do not include stats.";
+      let descriptionPrompt = "";
+
+      if (customDescription != "")
+      {
+          descriptionPrompt = "Generate a background for a character within the Star Frontiers sci-fi RPG called " + npc.name + " who is a " + npc.race + ". Here are your specific instructions: " + customDescription + ". Generate and include nothing more than description and keep it under" + descriptionLength + "words. DO NOT SAY YOU ARE MAKING A DESCRIPTION. Only generate a background. Do not include stats.";
+      }
+      else {
+          descriptionPrompt = "Generate a background for a character within the Star Frontiers sci-fi RPG called " + npc.name + " who is a " + npc.race + ". Generate and include nothing more than description and keep it under" + descriptionLength + "words. DO NOT SAY YOU ARE MAKING A DESCRIPTION. Only generate a background. Do not include stats.";
+      }
 
       try {
           const description = await ollama.chat({
               model: 'llama3.2:1b',
               messages: [{ role: 'user', content: descriptionPrompt }],
+              stream: true
           });
-
-          npc.description = description.message.content;
+          for await (const part of description) {
+              npc.description += part.message.content;
+          }
       } catch (error) {
           console.error("Error fetching response:", error);
       }
+
+      for (let i = 0; i < numInventory; i++) {
+          try {
+              const inventory = await ollama.chat({
+                  model: 'llama3.2:1b',
+                  messages: [{ role: 'user', content: lootPrompt }],
+              });
+
+              npc.inventory = [...npc.inventory, inventory.message.content];
+          } catch (error) {
+              console.error("Error fetching response:", error);
+          }
+      }
+
+      for (let i = 0; i < numSkills; i++) {
+          try {
+              const skills = await ollama.chat({
+                  model: 'llama3.2:1b',
+                  messages: [{ role: 'user', content: skillPrompt }],
+              });
+
+              npc.skills = [...npc.skills, skills.message.content];
+          } catch (error) {
+              console.error("Error fetching response:", error);
+          }
+      }
+
+      for (let i = 0; i < numWeapons; i++) {
+          try {
+              const weapons = await ollama.chat({
+                  model: 'llama3.2:1b',
+                  messages: [{ role: 'user', content: weaponPrompt }],
+              });
+
+              npc.weapons = [...npc.weapons, weapons.message.content];
+          } catch (error) {
+              console.error("Error fetching response:", error);
+          }
+      }
   }
+
+
+  //Experimental code
+
+  let currentModel = 'No Model Selected';
+  let showModelDropdown = false;
+  let modelWarn = false;
+  let isGenerating = false;
+  let modelNames: string[] = [];
+
+  //handle ollama models
+  async function ollamaModels() {
+      const ollamaModel = await ollama.list();
+      modelNames = ollamaModel.models.map((model: any) => model.name);
+  }
+
+  function selectModel(button: any) {
+      console.log(button)
+      currentModel = button
+      showModelDropdown = false;
+  }
+
+  function modelDropdown() {
+      showModelDropdown = !showModelDropdown;
+  }
+
+  onMount(() => {
+      ollamaModels();
+  })
 
 </script>
 
@@ -123,10 +222,14 @@
     <div class="flex-x h-[calc(100vh-65px)]">
         <div class="w-full p-6 flex gap-6 font-mono">
             <div class="w-full flex-y gap-4">
-                <div class="label">{npc.name}</div>
+                <div class="label-fixed justify-between">
+                    <div class="">{npc.name}</div>
+                    <div class="">{npc.age} yrs</div>
+                </div>
+
                 <div class="flex gap-4">
-                    <div class="label w-full">{npc.race}</div>
-                    <div class="label w-full">{npc.sex}</div>
+                    <div class="label-fixed w-full">{npc.race}</div>
+                    <div class="label-fixed w-full">{npc.sex}</div>
                 </div>
                 <div class="flex gap-4">
                     <div class="flex-y stat-label">
@@ -173,55 +276,77 @@
                     </div>
                 </div>
                 <div class="h-px w-full bg-gray-800"></div>
-                <div class="flex gap-4">
-                    <div class="flex-y stat-label">
-                        <div class="text-lg py-4 ">{npc.height}</div>
-                        <div class="h-px bg-gray-700 w-full"></div>
-                        <div class="text-[10px] py-2 text-gray-300">Height</div>
+                <div class="flex gap-4 w-full">
+                    <div class="outline flex w-full">
+                        <div class="text-[10px] text-gray-300 px-4 w-full p-2">Height</div>
+                        <div class="min-w-px bg-gray-800"></div>
+                        <div class="text-[10px] text-gray-300 px-4 w-full text-center p-2">{npc.height}</div>
                     </div>
-                    <div class="flex-y stat-label">
-                        <div class="text-lg py-4 ">{npc.weight}</div>
-                        <div class="h-px bg-gray-700 w-full"></div>
-                        <div class="text-[10px] py-2 text-gray-300">Weight</div>
-                    </div>
-                    <div class="flex-y stat-label">
-                        <div class="text-lg py-4 ">{npc.hitPoints}</div>
-                        <div class="h-px bg-gray-700 w-full"></div>
-                        <div class="text-[10px] py-2 text-gray-300">Hit Points</div>
-                    </div>
-                    <div class="flex-y stat-label">
-                        <div class="text-lg py-4 ">{npc.age}</div>
-                        <div class="h-px bg-gray-700 w-full"></div>
-                        <div class="text-[10px] py-2 text-gray-300">Age</div>
+                    <div class="outline flex w-full">
+                        <div class="text-[10px] text-gray-300 px-4 w-full p-2">Weight</div>
+                        <div class="min-w-px bg-gray-800"></div>
+                        <div class="text-[10px] text-gray-300 px-4 w-full text-center p-2">{npc.weight}</div>
                     </div>
                 </div>
-                <div class="flex gap-4">
-                    <div class="flex gap-4 w-full">
-                        <div class="flex-y stat-label">
-                            <div class="text-lg py-4 ">{npc.walk}</div>
-                            <div class="h-px bg-gray-700 w-full"></div>
-                            <div class="text-[10px] py-2 text-gray-300">Walk</div>
-                        </div>
-                        <div class="flex-y stat-label">
-                            <div class="text-lg py-4 ">{npc.run}</div>
-                            <div class="h-px bg-gray-700 w-full"></div>
-                            <div class="text-[10px] py-2 text-gray-300">Run</div>
-                        </div>
+                <div class="flex gap-4 w-full">
+                    <div class="outline flex w-full">
+                        <div class="text-[10px] text-gray-300 px-4 w-full p-2">Walk</div>
+                        <div class="min-w-px bg-gray-800"></div>
+                        <div class="text-[10px] text-gray-300 px-4 w-full text-center p-2">{npc.walk}</div>
                     </div>
-                    <div class="flex-y stat-label">
-                        <div class="text-lg py-4 ">{npc.credits}</div>
-                        <div class="h-px bg-gray-700 w-full"></div>
-                        <div class="text-[10px] py-2 text-gray-300">Credits</div>
+                    <div class="outline flex w-full">
+                        <div class="text-[10px] text-gray-300 px-4 w-full p-2">Run</div>
+                        <div class="min-w-px bg-gray-800"></div>
+                        <div class="text-[10px] text-gray-300 px-4 w-full text-center p-2">{npc.run}</div>
+                    </div>
+                </div>
+                <div class="flex gap-4 w-full">
+                    <div class="outline flex w-full">
+                        <div class="text-[10px] text-gray-300 px-4 w-full p-2">Health</div>
+                        <div class="min-w-px bg-gray-800"></div>
+                        <div class="text-[10px] text-gray-300 px-4 w-full text-center p-2">{npc.hitPoints}</div>
+                    </div>
+                    <div class="outline flex w-full">
+                        <div class="text-[10px] text-gray-300 px-4 w-full p-2">Credits</div>
+                        <div class="min-w-px bg-gray-800"></div>
+                        <div class="text-[10px] text-gray-300 px-4 w-full text-center p-2">{npc.credits}</div>
+                    </div>
+                </div>
+                <div class="label flex-y gap-2 h-full">
+                    <div class="flex-x justify-between">
+                        <div class="">Skills</div>
+                        <div class="">Lvl</div>
+                    </div>
+                    <div class="h-px bg-gray-800"></div>
+                    <div class="overflow-y-auto flex-y gap-2 h-full">
+                        {#each npc.skills as item}
+                            <div class="w-full flex-x justify-between">
+                                <div>{item}</div>
+                                <div>2</div>
+                            </div>
+                        {/each}
                     </div>
                 </div>
             </div>
             <div class="w-full flex-y gap-4">
-                <div class="label" id="description">{npc.description}</div>
-                <div class="label p-0 flex-y gap-2">
-                    <div class="">Inventory</div>
+                <div class="label overflow-y-auto h-full" id="description">{npc.description}</div>
+                <div class="label flex-y gap-2">
+                    <div class="">Inventory (Equipment)</div>
+                    <div class="h-px bg-gray-800"></div>
+                    <div class="overflow-y-auto flex-y gap-2 h-32">
+                        {#each npc.inventory as item}
+                            <div>{item}</div>
+                        {/each}
+                    </div>
                 </div>
-                <div class="label p-0 flex flex-y gap-2">
-                    <div class="">Skills</div>
+                <div class="label flex-y gap-2">
+                    <div class="">Weapons</div>
+                    <div class="h-px bg-gray-800"></div>
+                    <div class="overflow-y-auto flex-y gap-2 h-32">
+                        {#each npc.weapons as item}
+                            <div>{item}</div>
+                        {/each}
+                    </div>
                 </div>
             </div>
         </div>
@@ -249,31 +374,61 @@
                 </button>
             </div>
             <div class="h-px w-full bg-gray-800"></div>
-            <div class="flex m-4 gap-4">
-                <button class="button-hybrid w-full rounded-none text-center font-semibold font-mono flex justify-center">llama3.2:1b</button>
+            <div class="flex-y gap-4 m-4">
+                <div class="flex gap-4">
+                    <button on:click={modelDropdown()}  class="button-hybrid w-full rounded-none text-center font-semibold font-mono flex justify-center">{currentModel}</button>
 
-                <a class="icon-hybrid rounded-none" href="" aria-label="Youtube">
-                    <i class="bi bi-eject"></i>
-                </a>
-            </div>
-            <div class="h-px w-full bg-gray-800"></div>
-            <div class="h-full flex flex-y">
-                <div class="flex gap-3 flex-wrap m-4">
-                    <button on:click={setDifficulty(2)} class="hybrid tag">Easy</button>
-                    <button on:click={setDifficulty(4)} class="hybrid tag">Medium</button>
-                    <button on:click={setDifficulty(6)} class="hybrid tag">Hard</button>
-                    <button on:click={setDifficulty(8)} class="hybrid tag">Nightmare</button>
-                    <button on:click={setDifficulty(10)} class="hybrid tag">God</button>
-                    <button on:click={setDifficulty(2)} class="hybrid tag">Random</button>
+                    <a class="icon-hybrid rounded-none" href="" aria-label="Youtube">
+                        <i class="bi bi-eject"></i>
+                    </a>
                 </div>
-                <div class="flex gap-3 flex-wrap m-4">
+
+                {#if showModelDropdown}
+                    <div class="hybrid flex-y">
+                        {#each modelNames as names}
+                            <button on:click={() => selectModel(names)} class="tag">{names}</button>
+                        {/each}
+                    </div>
+                {/if}
+            </div>
+
+            <div class="h-px w-full bg-gray-800"></div>
+            <div class="h-full flex flex-y m-4 gap-5 overflow-y-auto">
+                <div class="flex gap-3 flex-wrap">
+                    <button class="hybrid tag">Easy</button>
+                    <button class="hybrid tag">Medium</button>
+                    <button class="hybrid tag">Hard</button>
+                    <button class="hybrid tag">Nightmare</button>
+                    <button class="hybrid tag">God</button>
+                    <button class="hybrid tag">Random</button>
+                </div>
+                <div class="flex gap-3 flex-wrap">
                     <button class="hybrid tag">Human</button>
                     <button class="hybrid tag">Yazarian</button>
                     <button class="hybrid tag">Vrusk</button>
                     <button class="hybrid tag">Dralasite</button>
                     <button class="hybrid tag">Random</button>
                 </div>
-                <input type="text" id="first_name" class="label m-4 font-mono placeholder-gray-500" placeholder="Custom Description">
+                <input type="text" id="first_name" class="label font-mono placeholder-gray-500 focus:outline-none" placeholder="Custom Description Prompt" bind:value={customDescription}>
+
+                <div class="flex-y gap-4">
+                    <div class="flex-x gap-3 font-mono">
+                        <div class="hybrid text-gray-300 h-10 text-xs w-[65%] flex items-center px-4">Equipment Amount</div>
+                        <input type="text" id="first_name" class="input placeholder-gray-500 text-center w-[35%] focus:outline-none" placeholder="#" bind:value={numInventory}>
+                    </div>
+                    <div class="flex-x gap-3 font-mono">
+                        <div class="hybrid text-gray-300 h-10 text-xs w-[65%] flex items-center px-4">Number of Skills</div>
+                        <input type="text" id="first_name" class="input placeholder-gray-500 text-center w-[35%] focus:outline-none" placeholder="#" bind:value={numSkills}>
+                    </div>
+                    <div class="flex-x gap-3 font-mono">
+                        <div class="hybrid text-gray-300 h-10 text-xs w-[65%] flex items-center px-4">Weapons Amount</div>
+                        <input type="text" id="first_name" class="input placeholder-gray-500 text-center w-[35%] focus:outline-none" placeholder="#" bind:value={numWeapons}>
+                    </div>
+                    <div class="flex-x gap-3 font-mono">
+                        <div class="hybrid text-gray-300 h-10 text-xs w-[65%] flex items-center px-4">Description Word Count</div>
+                        <input type="text" id="first_name" class="input placeholder-gray-500 text-center w-[35%] focus:outline-none" placeholder="#" bind:value={descriptionLength}>
+                    </div>
+                </div>
             </div>
             <div class="h-px w-full bg-gray-800"></div>
             <div class="p-4 flex">
